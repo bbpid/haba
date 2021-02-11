ba8815ca7b3545292dd9f7a7beedb206129283cb
Riza Yahya <4548351+gueriza@users.noreply.github.com>
2021-02-11 14:55:51 +0400

    f

--- haba/hooks.py
+++ haba/hooks.py
@@ -46,7 +46,7 @@ web_include_css = "/assets/haba/css/habamerg.css"
 # ----------
 
 # application home page (will override Website Settings)
-home_page = "login"
+# home_page = "login"
 
 # website user home page (by Role)
 # role_home_page = {
